629183f652395870369e40074e8cafda6ee15cb8
Diego Y. K <diegoy@gmail.com>
2012-06-05 00:12:49 -0300

    Refactored test code

--- test/com/runnerdtalks/mockitos/MyMessageSenderTest.java
+++ test/com/runnerdtalks/mockitos/MyMessageSenderTest.java
@@ -2,30 +2,34 @@ package com.runnerdtalks.mockitos;
 
 import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyMessageSenderTest {
 
+	private Connection connection;
+	private MyMessageSender messageSender;
+
+	@Before
+	public void before() {
+		connection = mock(Connection.class);
+		messageSender = new MyMessageSender(connection);
+	}
+	
 	@Test
 	public void sendMessageTest() {
-		Connection connection = mock(Connection.class);
-		
-		MyMessageSender messageSender = new MyMessageSender(connection);
-		
 		messageSender.send("Uma Mensagem");
 		
 		verify(connection).sendMessage("UMA MENSAGEM");
-		
 	}
 	
 	@Test
 	public void behaviourTest() {
-		Connection connection = mock(Connection.class);
-		
-		MyMessageSender messageSender = new MyMessageSender(connection);
-		
 		messageSender.sendMultipleMessages("Mensagem Um", "Mensagem Dois");
 		
 		verify(connection, times(2)).sendMessage(anyString());
+		verify(connection).sendMessage("MENSAGEM UM");
+		verify(connection).sendMessage("MENSAGEM DOIS");
 	}
+	
 }
